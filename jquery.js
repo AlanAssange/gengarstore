3eabd1e93fbb45da5f9104f276a934df9608fae5
Alan Cañellas <alanjc27@gmail.com>
2021-10-14 19:00:23 -0300

    añadí un nuevo js para efectos jquery

---
+++ jquery.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+    // Add smooth scrolling to all links
+    $("a").on('click', function(event) {
+  
+      if (this.hash !== "") {
+        event.preventDefault();
+  
+        const hash = this.hash;
+  
+        $('html, body').animate({
+          scrollTop: $(hash).offset().top
+        }, 600, function(){
+     
+          window.location.hash = hash;
+        });
+      } 
+    });
+  });

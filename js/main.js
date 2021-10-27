7b111a2039e70dded0a48eb0656627969b7c17a4
Alan Cañellas <alanjc27@gmail.com>
2021-10-27 20:14:15 -0300

    función constructora, nueva section

--- js/main.js
+++ js/main.js
@@ -6,3 +6,24 @@ document.querySelector('#menu-btn').onclick = () =>{
     searchForm.classList.remove('active');
     cartItem.classList.remove('active');
 }
+
+//objeto del día//
+
+let constructorObject = document.querySelector("#rawunique");
+
+
+class Pokemon {
+    constructor(name, price, inCarts) {
+        this.name = name;
+        this.price = parseFloat(price);
+        this.inCart = inCarts;
+    }
+}
+
+const mewtwotee = new Pokemon ("Mewtwo Tee", 20.99, "Scroll debajo para comprar!")
+
+constructorObject.innerHTML +=`
+            
+<p>${mewtwotee.name}</p>
+
+`
